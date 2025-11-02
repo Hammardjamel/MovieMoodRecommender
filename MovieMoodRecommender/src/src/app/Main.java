e076259eaf1dce6458cf3189df3d123bb724ff84
Hammardjamel <109036788+Hammardjamel@users.noreply.github.com>
2025-11-02 17:07:02 +0100

    Delete MovieMoodRecommender/src/src directory

--- MovieMoodRecommender/src/src/app/Main.java
+++
@@ -1,9 +0,0 @@
-package app;
-import ui.ConsoleUI;
-
-public class Main {
-    public static void main(String[] args) {
-        ConsoleUI consoleUI = new ConsoleUI();
-        consoleUI.startQuiz();
-    }
-}
